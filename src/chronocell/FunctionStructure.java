bfd8b3457dfcca92bf0437b691a42bad3e6e34de
goby <goby@localhost>
2017-01-24 15:08:25 +0100

    Creation of class FunctionStructure

---
+++ src/chronocell/FunctionStructure.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chronocell;
+
+/**
+ *
+ * @author goby
+ */
+public class FunctionStructure {
+    double min=0.0,max=0.0,step=1.0;
+    double[] values=null;    
+}
