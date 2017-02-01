fc525292be34b39b1348eb613bdddd84f589c36f
goby <goby@localhost>
2017-02-01 08:23:53 +0100

    Display two phases in GUISolution

--- src/chronocell/ChronoCell.java
+++ src/chronocell/ChronoCell.java
@@ -15,51 +15,42 @@ public class ChronoCell {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Numbers.minStep=0.00001;
         // Parameters
-            SolutionStructure sol=Operators.createSolutionStructure(1);
+            SolutionStructure sol=Operators.createSolutionStructure(2);
             sol.phaseName[0]="G1/S";
+            sol.phaseName[1]="G2/M";
         ///// Initial conditions phase G1/S
             sol.theta[0]= Operators.createFunction(Numbers.CGN(0.0),Numbers.CGN(1.0),Numbers.CGN(0.01));
             Operators.MapFunctionValues(sol.theta[0],0.5,1.0,Operators.constant);
             ///// Transition function first checkpoint
             sol.transitionProbabilities[0]=Operators.createFunction(Numbers.CGN(0.0),Numbers.CGN(1.0),Numbers.CGN(0.01)); 
-            Operators.MapFunctionValues(sol.transitionProbabilities[0],0.6,1.0,Operators.sinPeriodOne);
+            Operators.MapFunctionValues(sol.transitionProbabilities[0],0.99,1.0,Operators.constant);
             sol.transitionProbabilities[0]=Operators.AffineFunctionTransformation(1/Operators.IntegrateFunction(sol.transitionProbabilities[0], sol.transitionProbabilities[0].min, sol.transitionProbabilities[0].max),0, sol.transitionProbabilities[0]);
-            ///// Cumulative function first checkpoint
-//            Operators.PrintFunction("trans", (sol.transitionProbabilities[0]));
-//            Operators.PrintFunction("cumul", Operators.CumulativeFunction(sol.transitionProbabilities[0]));
+        ///// Cumulative function first checkpoint
             sol.oneMinusCumulativeFunctions[0]=Operators.AffineFunctionTransformation(-1.0, 1.0,Operators.CumulativeFunction(sol.transitionProbabilities[0]));
         ///// Initial conditions phase G2/M 
-//            sol.phaseName[1]="G2/M";
-//            sol.theta[1]= Operators.createFunction(Numbers.CGN(0.0),Numbers.CGN(1.0),Numbers.CGN(0.01));
-//            Operators.MapFunctionValues(sol.theta[1],Operators.constant);
-////             sol.theta[1]=Operators.AffineFunctionTransformation(0.0,0.0,sol.theta[1]);
-//            ///// Transition function first checkpoint
-//            sol.transitionProbabilities[1]=Operators.createFunction(Numbers.CGN(0.5),Numbers.CGN(1.0),Numbers.CGN(0.01)); 
-//            Operators.MapFunctionValues(sol.transitionProbabilities[1],Operators.constant);
-//            sol.transitionProbabilities[1]=Operators.AffineFunctionTransformation(1/Operators.IntegrateFunction(sol.transitionProbabilities[1], sol.transitionProbabilities[1].min, sol.transitionProbabilities[1].max),0, sol.transitionProbabilities[1]);
-//            ///// Cumulative function first checkpoint
-//            sol.cumulativeFunctions[1]=Operators.CumulativeFunction(sol.transitionProbabilities[1]);
-            // Simulation
-        for (int i=0;i<500;i++){            
+            sol.theta[1]= Operators.createFunction(Numbers.CGN(0.0),Numbers.CGN(1.0),Numbers.CGN(0.01));
+            Operators.MapFunctionValues(sol.theta[1],0.0,0.0,Operators.constant);
+            ///// Transition function first checkpoint
+            sol.transitionProbabilities[1]=Operators.createFunction(Numbers.CGN(0.0),Numbers.CGN(1.0),Numbers.CGN(0.01)); 
+            Operators.MapFunctionValues(sol.transitionProbabilities[1],0.6,1.0,Operators.sinPeriodOne);
+            sol.transitionProbabilities[1]=Operators.AffineFunctionTransformation(1/Operators.IntegrateFunction(sol.transitionProbabilities[1], sol.transitionProbabilities[1].min, sol.transitionProbabilities[1].max),0, sol.transitionProbabilities[1]);
+        ///// Cumulative function first checkpoint
+            sol.oneMinusCumulativeFunctions[1]=Operators.AffineFunctionTransformation(-1.0, 1.0,Operators.CumulativeFunction(sol.transitionProbabilities[1]));
+         // Simulation
+        for (int i=0;i<10000;i++){            
             Operators.ComputeSolutionNextValue(sol);
 //            Operators.PrintFunction(dst.ageDistribution[0]);
 //            System.err.format("intégrale = %f \n",Operators.IntegrateFunction(dst.ageDistribution[0], dst.ageDistribution[0].min, dst.ageDistribution[0].min+1.0));
         }
 //         Display Function
-        FunctionStructure indicatrice=Operators.createFunction(0.0, 1.0, 0.1);
-        Operators.MapFunctionValues(indicatrice, 0.0, 1.0, Operators.constant);
-        Operators.PrintFunction("IND",indicatrice);
-        Operators.PrintFunction("INDTranslatée",Operators.TranslateFunction(0.5,indicatrice));
-        GUI win1 =new GUI();    
-        win1.SetFunction(sol.theta[0]);
-        win1.setVisible(true);
-        GUI win2 =new GUI();
+//        GUI win1 =new GUI();    
+//        win1.SetFunction(sol.theta[0]);
+//        win1.setVisible(true);
+//        GUI win2 =new GUI();
 //        win2.SetFunction(sol.theta[0]);
 //        win2.setVisible(true);
-////        Operators.PrintFunction(sol.theta[0]);
         GUISolution win3 =new GUISolution();
         win3.SetFunction(sol);
         win3.setVisible(true);
--- src/chronocell/GUISolution.java
+++ src/chronocell/GUISolution.java
@@ -33,9 +33,12 @@ public class GUISolution extends javax.swing.JFrame {
         jSlider1 = new javax.swing.JSlider();
         lblYMax = new javax.swing.JLabel();
         lblPhaseName = new javax.swing.JLabel();
-        lblYMin = new javax.swing.JLabel();
         lblXMin = new javax.swing.JLabel();
         lblXMax = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,11 +48,11 @@ public class GUISolution extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 595, Short.MAX_VALUE)
+            .addGap(0, 619, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 374, Short.MAX_VALUE)
+            .addGap(0, 398, Short.MAX_VALUE)
         );
 
         jSlider1.setMaximum(1000);
@@ -64,35 +67,63 @@ public class GUISolution extends javax.swing.JFrame {
 
         lblPhaseName.setText("jLabel5");
 
-        lblYMin.setText("jLabel2");
-
         lblXMin.setText("jLabel3");
 
         lblXMax.setText("jLabel4");
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 625, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jLabel1.setText("jLabel1");
+
+        jLabel2.setText("jLabel2");
+
+        jLabel3.setText("jLabel3");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(62, 62, 62)
-                .addComponent(lblYMin)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(lblXMin)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblXMax))
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblYMax)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(lblPhaseName, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(202, 202, 202))
+                        .addGap(178, 178, 178)
+                        .addComponent(lblPhaseName, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(49, 49, 49)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel1)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblXMin)
-                                .addGap(421, 421, 421)
-                                .addComponent(lblXMax))
-                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addContainerGap(72, Short.MAX_VALUE))))
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2))
+                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 37, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(371, 371, 371))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -100,17 +131,21 @@ public class GUISolution extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lblPhaseName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblYMax))
+                    .addComponent(lblYMax)
+                    .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lblYMin, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblXMax)
                     .addComponent(lblXMin)
-                    .addComponent(lblXMax))
-                .addGap(18, 18, 18)
-                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -164,37 +199,47 @@ public class GUISolution extends javax.swing.JFrame {
     @Override
     public void paint(Graphics g){
         super.paint(g);
-        Graphics gp=jPanel1.getGraphics();        
-        FunctionStructure indicatrice=Operators.createFunction(0.0, 1.0, 0.01);
-        Operators.MapFunctionValues(indicatrice,0.0, 1.0, Operators.constant);
-        FunctionStructure tempFunction =Operators.MultiplyFunctionRaw(f.oneMinusCumulativeFunctions[0],Operators.TranslateFunction(jSlider1.getValue()*f.theta[0].step,f.theta[0]));
-//      FunctionStructure tempFunction =Operators.MultiplyFunctionRaw(f.oneMinusCumulativeFunctions[0],Operators.TranslateFunction(jSlider1.getValue()*f.theta[0].step,f.theta[0]));
-//        Operators.PrintFunction("tempInGuiSOl",tempFunction);
+        Graphics gp1=jPanel1.getGraphics();
+        FunctionStructure tempFunction1 =Operators.MultiplyFunctionRaw(f.oneMinusCumulativeFunctions[0],Operators.TranslateFunction(jSlider1.getValue()*f.theta[0].step,f.theta[0]));
         System.err.format("translation de: %f\n",jSlider1.getValue()*f.theta[0].step);
-//        double minVal=Operators.GetFunctionMinValue(tempFunction);
-        double minVal=0;
-        double maxVal=Operators.GetFunctionMaxValue(tempFunction);
-        for (int i=tempFunction.minIndex;i<=tempFunction.maxIndex-1;i++){
-            int absciss1=(int) Math.round((i-tempFunction.minIndex)*jPanel1.getWidth()/(tempFunction.maxIndex-tempFunction.minIndex+1));
-            int absciss2=(int) Math.round((i-tempFunction.minIndex+1)*jPanel1.getWidth()/(tempFunction.maxIndex-tempFunction.minIndex+1));
-            int ordinate1=(int) Math.round((jPanel1.getHeight()/(minVal-maxVal))*(tempFunction.values[i]-maxVal));
-            int ordinate2=(int) Math.round(jPanel1.getHeight()/(minVal-maxVal)*(tempFunction.values[i+1]-maxVal));
-            gp.drawLine(absciss1,ordinate1, absciss2,ordinate2);
+        double minVal1=0;
+        double maxVal1=Math.max(1, Operators.GetFunctionMaxValue(tempFunction1));
+        for (int i=tempFunction1.minIndex;i<=tempFunction1.maxIndex-1;i++){
+            int absciss11=(int) Math.round((i-tempFunction1.minIndex)*jPanel1.getWidth()/(tempFunction1.maxIndex-tempFunction1.minIndex+1));
+            int absciss12=(int) Math.round((i-tempFunction1.minIndex+1)*jPanel1.getWidth()/(tempFunction1.maxIndex-tempFunction1.minIndex+1));
+            int ordinate11=(int) Math.round((jPanel1.getHeight()/(minVal1-maxVal1))*(tempFunction1.values[i]-maxVal1));
+            int ordinate12=(int) Math.round(jPanel1.getHeight()/(minVal1-maxVal1)*(tempFunction1.values[i+1]-maxVal1));
+            gp1.drawLine(absciss11,ordinate11, absciss12,ordinate12);
+        }    
+        
+        Graphics gp2=jPanel2.getGraphics();
+        FunctionStructure tempFunction2 =Operators.MultiplyFunctionRaw(f.oneMinusCumulativeFunctions[1],Operators.TranslateFunction(jSlider1.getValue()*f.theta[1].step,f.theta[1]));
+        System.err.format("translation de: %f\n",jSlider1.getValue()*f.theta[1].step);
+        double minVal2=0;
+        double maxVal2=Operators.GetFunctionMaxValue(tempFunction2);
+        for (int i=tempFunction2.minIndex;i<=tempFunction2.maxIndex-1;i++){
+            int absciss1=(int) Math.round((i-tempFunction2.minIndex)*jPanel1.getWidth()/(tempFunction2.maxIndex-tempFunction2.minIndex+1));
+            int absciss2=(int) Math.round((i-tempFunction2.minIndex+1)*jPanel1.getWidth()/(tempFunction2.maxIndex-tempFunction2.minIndex+1));
+            int ordinate1=(int) Math.round((jPanel1.getHeight()/(minVal2-maxVal2))*(tempFunction2.values[i]-maxVal2));
+            int ordinate2=(int) Math.round(jPanel1.getHeight()/(minVal2-maxVal2)*(tempFunction2.values[i+1]-maxVal2));
+            gp2.drawLine(absciss1,ordinate1, absciss2,ordinate2);
         }
-        lblXMin.setText((new Double(tempFunction.min)).toString());
-        lblXMax.setText((new Double(tempFunction.max)).toString());
-        lblYMin.setText((new Double(minVal)).toString());
-        lblYMax.setText((new Double(maxVal)).toString());
+        lblXMin.setText((new Double(tempFunction1.min)).toString());
+        lblXMax.setText((new Double(tempFunction1.max)).toString());
+        lblYMax.setText((new Double(maxVal1)).toString());
         lblPhaseName.setText(f.phaseName[0]);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JSlider jSlider1;
     private javax.swing.JLabel lblPhaseName;
     private javax.swing.JLabel lblXMax;
     private javax.swing.JLabel lblXMin;
     private javax.swing.JLabel lblYMax;
-    private javax.swing.JLabel lblYMin;
     // End of variables declaration//GEN-END:variables
 }
