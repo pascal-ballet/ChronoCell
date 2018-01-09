924524a0c153fe99a244293d8ad5349e6c902450
goby <goby@localhost>
2018-01-09 14:03:14 +0100

    Dans SurvivalProbabilities, fonction survivalProbabilities à tester

--- src/chronocell/SurvivalProbabilities.java
+++ src/chronocell/SurvivalProbabilities.java
@@ -17,18 +17,39 @@ import java.util.Hashtable;
  */
 public class SurvivalProbabilities {
     //-------------- Probabilités de survie (lecture dans un .csv)
-// Chaque colone correspond à un dosage, chaque ligne à un temps dans le cycle
+// Chaque ligne correspond à un dosage, chaque colonne à un temps dans le cycle
 // On décompose ces données en une courbe pour chaque phase, en fonction de son avancement (donc une courbe définie sur [0,1]
 // que l'on va ensuite transformer en fonction du temps aléatoire passé dans la phase
-       public static void survivalProbabilities(double dose,int phase,CellDynamics dynamics, FunctionStructure curve){
+       public static FunctionStructure survivalProbabilities(double dose,int phase,CellDynamics dynamics, SurvivalDataStructure data){
            // il faut ici utiliser la distribution de la durée de la phase, à définir dans dynamics
            FunctionStructure duree= Operators.copyFunction(dynamics.getPhase(phase).timeDensity); 
            FunctionStructure OneMinCumulDuree= Operators.copyFunction(dynamics.getPhase(phase).timeOneMinCumul); 
            // créer la piecewise issue des data 
-           for (int i=curve.minIndex;i<curve.maxIndex;i++){
-               
+           // On commence par situer le dosage dans le tableau
+           int iDose=0;
+           while(dose>data.getPhase(phase)[iDose][0]) iDose++;
+           iDose--;
+           // la dose est comprise entre la ligne i et la ligne i+1
+           // à terme il faudrait interpoler, mais on peut commencer simplement par une approximation de la dose par celle de la ligne i
+           double[] p=new double[(data.getPhase(phase)[0].length-1)*2];
+           for (int j=0;j<p.length;j++){
+               p[2*j]=data.getPhase(phase)[0][j];
+               p[2*j+1]=data.getPhase(phase)[iDose][j];
            }
-           Operators.curve
+           //FunctionStructure survivalProbabilitiesData=Operators.createFunction(0.0, 1.0, duree.step);
+           //Operators.MapFunctionValues(survivalProbabilitiesData, 0.0, 1.0, Operators.piecewiseLinear, p);
+           FunctionStructure survival=Operators.createFunction(duree.min,duree.max,duree.step);
+           for (double t=survival.min;t<=survival.max;t=t+survival.step){
+                FunctionStructure temp=Operators.AffineFunctionTransformation(1/Operators.GetFunctionValue(OneMinCumulDuree,t),0.0,duree);
+                // création de la fonction t/s (on pourrait créer 1/s et en prendre des trasnformation affine, mais le gain n'est pas clair
+                FunctionStructure homo=Operators.createFunction(t, duree.max, duree.step);
+                Operators.MapFunctionValues(homo, homo.min,homo.max , Operators.homographie, 0.0,t,1.0,0.0);
+                temp=Operators.MultiplyFunctions(Operators.ComposeFunctionInterfaceFunctionStructure(homo, Operators.piecewiseLinear, p),temp);
+                survival.values[i]=Operators.IntegrateFunction(temp, t, temp.max);
+           }
+           return survival;
+       }
+       
        }
 //    
 //        Hashtable<String,FunctionStructure> survivalRaw = new Hashtable<String,FunctionStructure>(); 
